d4f227c188bb6dc154f826ba46d324d25431b3a0
SaRu-N <saruniroula73@gmail.com>
2022-06-16 22:15:08 +0545

    Delegates and Events done

---
+++ Deligates.cs
@@ -0,0 +1,83 @@
+namespace NCClabs{
+    public class Delegates{
+ public delegate void MathOps(int a, int b);
+
+        public event MathOps MathHandler;
+
+        public void Run(int a, int b)
+        {
+            MathHandler.Invoke(a, b);
+        }
+
+        public void Run()
+        {
+            CallingAnonymousFunction();
+
+            UnicasDelegate();
+
+            MulticastDelegate();
+        }
+
+        private void CallingAnonymousFunction()
+        {
+            MathOps m = new MathOps(Add);
+            m(10, 5);
+            m = (int x, int y) =>
+            {
+                Console.WriteLine($"Remainder = {x % y}");
+            };
+            m(10, 5);
+        }
+
+        private void UnicasDelegate()
+        {
+            //unicast delegate
+            MathOps m = new MathOps(Add);
+            m(2, 3);
+            m = Subtract;
+            m(2, 3);
+            m = Multiply;
+            m(2, 3);
+            m = Divide;
+            m(2, 3);
+        }
+
+        private void MulticastDelegate()
+        {
+            //multicast delegate
+            MathOps math = Add;
+            math += Subtract;
+            math += Multiply;
+            math += Divide;
+            math += Add;
+            math(10, 5);
+
+            math -= Add;
+            math -= Multiply;
+            math(10, 10);
+
+            math -= Multiply;
+            math(10, 10);
+        }
+
+        public void Add(int a, int b)
+        {
+            Console.WriteLine($"Sum = {a + b}");
+        }
+
+        public void Subtract(int a, int b)
+        {
+            Console.WriteLine($"Difference = {a - b}");
+        }
+
+        public void Multiply(int a, int b)
+        {
+            Console.WriteLine($"Product = {a * b}");
+        }
+
+        public void Divide(int a, int b)
+        {
+            Console.WriteLine($"Quotient = {a / b}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -18,7 +18,8 @@ namespace NCClabs
             // PartialExample()
             // InheritenceExample()
             // MultipleInheritance();
-            StackImplementation();
+            // StackImplementation();
+            // DelegateExample();
                
      System.Console.WriteLine("Do you want to continue?(Y/N):");
         choice= Console.ReadLine();
@@ -289,5 +290,23 @@ private static void ShapesExample()
             LivingThings.TemplatedFunction<string, IShape, float>("some string", new Circle(), (float)d);
         }
 
+private static void DelegateExample()
+        {
+            Delegates ds = new Delegates();
+            ds.MathHandler += Ds_MathHandler;
+            ds.Run(2, 3);
+            Delegates ds1 = new Delegates();
+            ds1.MathHandler += Ds1_MathHandler;
+            ds1.Run(4, 5);
+        }
+ private static void Ds1_MathHandler(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+private static void Ds_MathHandler(int a, int b)
+        {
+            Console.WriteLine($"Function running from program.cs with parameters a={a} and b={b} ");
+        }
 }
 }
